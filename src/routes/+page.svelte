c586112c432d00bd7cfc2cbec08bd99348177ec0
Neville Brem <bremneville@gmail.com>
2024-03-20 19:50:44 +0100

    added a new theme and tweaked the hero a tiny bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
-<div
-	id="hero"
-	class="w-full h-[40dvh] p-16 rounded-token text-primary flex flex-col justify-between"
->
+<div id="hero" class="rounded-token flex h-[40dvh] w-full flex-col justify-between p-16">
 	<div>
 		<h1 class="text-5xl font-black">I love to take pictures</h1>
 		<h3 class="text-xl font-bold">Here are some of them</h3>
 	</div>
-	<a href="/gallery" class="btn btn-primary text-primary-content mx-auto">Explore the gallery</a>
+	<a href="/gallery" class="btn btn-primary mx-auto">Explore the gallery</a>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,21 @@ const config = {
 	plugins: [require('daisyui'), require('@tailwindcss/typography')],
 
 	daisyui: {
-		themes: ['night']
+		themes: [
+			{
+				modern: {
+					primary: '#00d8d0', // Rich Electric Blue
+					secondary: '#6b38fb', // Cyan (Process)
+					accent: '#fcbf1e', // Yellow (Crayola)
+					neutral: '#2d3748', // Blue Gray (Dark)
+					'base-100': '#1a202c', // Jet
+					info: '#63b3ed', // Light Steel Blue
+					success: '#4caf50', // Green (Strong)
+					warning: '#ff9800', // Orange (Strong)
+					error: '#f44336' // Red (Strong)
+				}
+			}
+		]
 	}
 };
 
