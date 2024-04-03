b11fac7f903183c3689a39a58a252a07360d888c
Neville Brem <bremneville@gmail.com>
2024-04-03 16:42:00 +0200

    changed adapter to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import db from "@astrojs/db";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), db()],
   output: "static",
-  adapter: vercel()
+  adapter: vercel(),
 });
