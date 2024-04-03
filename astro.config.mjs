e62edb5fe8fd57ff24dcbd2b2f78a51ecb1003b4
Neville Brem <bremneville@gmail.com>
2024-04-03 16:41:08 +0200

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), db()]
+  integrations: [tailwind(), svelte(), db()],
+  output: "static",
+  adapter: vercel()
 });
