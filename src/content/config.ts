91f81096458f444303ca79ed6c730e27aa35b8e3
Neville Brem <bremneville@gmail.com>
2024-06-04 15:08:22 +0200

    added blog and first image

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.date(),
+    author: z.string(),
+    desc: z.string(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,10 +14,6 @@ export default {
           accent: "#fcbf1e", // Yellow (Crayola)
           neutral: "#2d3748", // Blue Gray (Dark)
           "base-100": "#1a202c", // Jet
-          info: "#63b3ed", // Light Steel Blue
-          success: "#4caf50", // Green (Strong)
-          warning: "#ff9800", // Orange (Strong)
-          error: "#f44336", // Red (Strong)
         },
       },
     ],
