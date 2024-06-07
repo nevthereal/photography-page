bb8498c274498a94078bded1cf3cd1a70b692d9e
Neville Brem <bremneville@gmail.com>
2024-06-07 21:03:09 +0200

    removed astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import db from "@astrojs/db";
 
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), db()],
+  integrations: [tailwind(), svelte()],
   output: "static",
   adapter: vercel(),
 });
