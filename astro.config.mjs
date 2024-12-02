d5b4c53b14d06a38f593a33d446eed4b63ced7dc
Neville Brem <bremneville@gmail.com>
2024-12-02 07:45:09 +0100

    moved to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/static";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   output: "static",
-  adapter: vercel(),
+  adapter: vercel()
 });
